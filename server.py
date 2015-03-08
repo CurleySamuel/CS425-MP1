993367e2c04e8d4837ed09f92ef4044c4477c149
Sam Curley <CurleySamuel@gmail.com>
2015-03-07 19:20:39 -0500

    Added a new thread to send all messages leaving the main thread simply input

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import datetime
 import signal
 import atexit
 import os
+from multiprocessing.queues import SimpleQueue
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,19 +19,15 @@ class bcolors:
     ENDC = '\033[0m'
 
 def signal_handler(signal, frame):
-
-    print(bcolors.FAIL +  'Force Quit' + bcolors.ENDC)
     s.close()
     exit(0)
 
 def readFile(fileName):
-
     with open(fileName) as f:
         commands = f.read().splitlines()
     return commands
 
 def listeningThread(listenIP, listenPort, bufferSize):
-
     global s
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -47,6 +44,18 @@ def listeningThread(listenIP, listenPort, bufferSize):
         #conn.send('ACK')
     conn.close()
 
+def worker_thread(TCP_IP, TCP_SENDPORT, message_queue):
+    while 1:
+        messages = message_queue.get()
+        for message in messages:
+            s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s2.connect((TCP_IP, TCP_SENDPORT))
+            s2.send(message)
+            #data = s2.recv(BUFFER_SIZE) #Recieve ACK
+            print bcolors.OKBLUE +  'Sent "' + message + '", system time is ' + \
+                str(datetime.datetime.now().time().strftime("%H:%M:%S")) + bcolors.ENDC
+            s2.close()
+
 def main():
 
     signal.signal(signal.SIGINT, signal_handler)
@@ -56,27 +65,24 @@ def main():
     TCP_RECEIVEPORT = int(sys.argv[2])
     BUFFER_SIZE = 1024
 
-    listener = threading.Thread(target=listeningThread, args=[TCP_IP, TCP_RECEIVEPORT, BUFFER_SIZE])
+    listener = threading.Thread(target=listening_thread, args=[TCP_IP, TCP_RECEIVEPORT, BUFFER_SIZE])
     listener.daemon = True
     listener.start()
+
+    message_queue = SimpleQueue()
+
+    worker = threading.Thread(target=worker_thread, args=[TCP_IP, TCP_SENDPORT, message_queue])
+    worker.daemon = True
+    worker.start()
 
     while 1:
         command = str(raw_input(bcolors.HEADER +  bcolors.UNDERLINE + "Enter Message:\n" + bcolors.ENDC))
         messages = []
-
-        if '.txt' in command:
+        if command.endswith('.txt'):
             messages = readFile(command)
         else:
             messages.append(command)
-
-        for message in messages:
-            s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s2.connect((TCP_IP, TCP_SENDPORT))
-            s2.send(message)
-            #data = s2.recv(BUFFER_SIZE) #Recieve ACK
-            print bcolors.OKBLUE +  'Sent "' + message + '", system time is ' + \
-                str(datetime.datetime.now().time().strftime("%H:%M:%S")) + bcolors.ENDC
-            s2.close()
+        message_queue.put(messages)
 
 if __name__ == "__main__":
     main()
